49ca099d80b4d179b762d11f7ff7ce589efe3488
Miuniverso <m.yana93@mail.ru>
2020-02-25 14:04:39 +0300

    Ошибка при нажатии на кнопку "Попробовать снова" + отсутствие описания ошибки

--- js/server-request.js
+++ js/server-request.js
@@ -2,21 +2,39 @@
 
 (function () {
   var posters = [];
+  var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+  var main = document.querySelector('main');
 
   var request = new XMLHttpRequest();
-  request.open('GET', 'https://js.dump.academy/keksobooking/data', true);
+  request.open('GET', 'https://js.dump.academy/keksoboking/data', true);
+
+  // отображение ошибок на странице
   request.addEventListener('load', onLoad);
-  request.addEventListener('error', onError);
-  // потом заменю консоль лог
-  function onError(msg) {
-    console.log('Сработало следующая ошибка: ' + msg);
+  request.addEventListener('error', showErrorMessage);
+
+  function closeError() {
+    main.removeChild(document.querySelector('.error'));
+  }
+
+  // появление окна с ошибкой
+  function showErrorMessage(msg) {
+
+    var errorClone = errorTemplate.cloneNode(true);
+    var fragment = document.createDocumentFragment();
+
+    errorClone.querySelector('.error__message').innerHTML = msg;
+    fragment.appendChild(errorClone);
+    main.appendChild(fragment);
+
+    // Закрытие окна с ошибкой
+    errorClone.querySelector('.error__button').addEventListener('click', closeError);
+    window.addEventListener('click', closeError);
   }
 
   function onLoad() {
     if (request.status !== 200) {
-      console.log('Ответ' + request.status + ': ' + request.statusText);
-    } else {
-      console.log('Успешная загрузка');
+      var errorMessage = 'Ошибка: ' + request.status + ' - ' + request.statusText;
+      showErrorMessage(errorMessage);
     }
   }
 
